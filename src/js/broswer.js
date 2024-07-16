af6bdfa4e18e3f546013663ce9564bb6c8cd6aae
SodiumCode <3409473369@qq.com>
2024-07-16 17:04:11 +0800

    feat: 网页迁移提示

--- src/js/broswer.js
+++ src/js/broswer.js
@@ -1,10 +1,15 @@
+// 首先检测是否为Safari浏览器且版本低于13
 const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
-
 if (isSafari) {
     const userAgent = navigator.userAgent;
-    const safariVersion = parseInt(userAgent.match(/Version\/(\d+)/)[1]);
-
-    if (safariVersion < 13) {
+    const safariVersionMatch = userAgent.match(/Version\/(\d+)/);
+    if (safariVersionMatch && parseInt(safariVersionMatch[1]) < 13) {
         window.location.href = "/src/page/broswer.html";
     }
+}
+
+// 接下来检测当前域名是否为uef.zaona.top
+const currentDomain = window.location.hostname;
+if (currentDomain === 'uef.zaona.top') {
+    window.location.href = "/src/page/move.html";
 }
