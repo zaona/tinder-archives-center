587cbc5f2cece7919ba52008f885ca0c3426ba02
SodiumCode <3409473369@qq.com>
2024-07-17 00:50:38 +0800

    feat: color

--- pages/Color.vue
+++ pages/Color.vue
@@ -1,6 +1,64 @@
 <template>
-  <div>
-    color
+  <div class="bg-no-repeat bg-cover bg-center bg-[url('~/assets/img/pages-banner/bg-color.png')] w-full h-[160px] mb-12">
+    <div class="bg-gradient-to-r from-black/50 h-full">
+      <div class="h-full max-w-[1200px] mx-auto pl-4">
+        <p class="text-[48px] pt-[56px] text-white leading-[48px]">颜色</p>
+      </div>
+    </div>
+  </div>
+  <div class="mx-auto max-w-[1200px] px-4 mb-12">
+    <div class="mb-12">
+      <p class="text-[36px]">流浪地球</p>
+      <div class="flex items-center text-[14px] mt-[16px]">
+        <img class="mr-2" src="~/assets/icon/info.svg">来自：哔哩哔哩 · @长期洗涤Longwashing
+      </div>
+      <div class="grid lg:grid-cols-5 grid-cols-2 gap-[16px] mt-[16px]">
+        <div class="bg-white items-center px-[16px] py-[16px] border-solid border transition-shadow hover:shadow-lg">
+          <div class="flex flex-row justify-between h-[56px]">
+            <p class="text-[16px] font-bold">红-1</p>
+            <span class="w-[24px] h-[24px] bg-[#EC2021]"></span>
+          </div>
+          <p class="text-[16px] text-gray-500">#EC2021</p>
+        </div>
+        <div class="bg-white items-center px-[16px] py-[16px] border-solid border transition-shadow hover:shadow-lg">
+          <div class="flex flex-row justify-between h-[56px]">
+            <p class="text-[16px] font-bold">红-2</p>
+            <span class="w-[24px] h-[24px] bg-[#FA3E00]"></span>
+          </div>
+          <p class="text-[16px] text-gray-500">#FA3E00</p>
+        </div>
+      </div>
+    </div>
+    <div class="mb-12">
+      <p class="text-[36px]">地球联合政府</p>
+      <div class="flex items-center text-[14px] mt-[16px]">
+        <img class="mr-2" src="~/assets/icon/info.svg">来自：UET设计组校准
+      </div>
+      <div class="grid lg:grid-cols-5 grid-cols-2 gap-[16px] mt-[16px]">
+        <div class="bg-white items-center px-[16px] py-[16px] border-solid border transition-shadow hover:shadow-lg">
+          <div class="flex flex-row justify-between h-[56px]">
+            <p class="text-[16px] font-bold">浅蓝</p>
+            <span class="w-[24px] h-[24px] bg-[#267F9D]"></span>
+          </div>
+          <p class="text-[16px] text-gray-500">#267F9D</p>
+        </div>
+      </div>
+    </div>
+    <div class="mb-12">
+      <p class="text-[36px]">北京航天中心</p>
+      <div class="flex items-center text-[14px] mt-[16px]">
+        <img class="mr-2" src="~/assets/icon/info.svg">来自：机核 · 流浪地球美术设定
+      </div>
+      <div class="grid lg:grid-cols-5 grid-cols-2 gap-[16px] mt-[16px]">
+        <div class="bg-white items-center px-[16px] py-[16px] border-solid border transition-shadow hover:shadow-lg">
+          <div class="flex flex-row justify-between h-[56px]">
+            <p class="text-[16px] font-bold">深蓝</p>
+            <span class="w-[24px] h-[24px] bg-[#22408F]"></span>
+          </div>
+          <p class="text-[16px] text-gray-500">#22408F</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -15,6 +73,4 @@ useHead({
 })
 </script>
 
-<style>
-
-</style>
+<style></style>
