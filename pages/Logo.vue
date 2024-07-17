726fcc61f632da39a57ad259d216ffcee9c561de
SodiumCode <3409473369@qq.com>
2024-07-17 23:57:54 +0800

    feat: 标志文件

--- pages/Logo.vue
+++ pages/Logo.vue
@@ -2,7 +2,7 @@
   <div>
     <Banner bgImage="/img/pages-banner/bg-logo.png" bannerTitle="标志[ING]" />
 
-    <div class="mx-auto mb-12 max-w-[1200px] px-4 py-12">
+    <div class="mx-auto max-w-[1200px] px-4 py-12">
       <div class="border border-solid bg-gray-50 p-6 lg:p-8">
         <p class="leading-loose">
           以下所有标志的修复底图都取自「流浪地球美术设定」和「流浪地球2制作手记」
