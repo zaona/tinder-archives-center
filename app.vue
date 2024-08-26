23f9ef4940260b5489748dcc448723abd8958f38
simplenty <lishangmingxuan@foxmail.com>
2024-08-26 11:13:33 +0800

    upd: 加入半透明滚动条

--- app.vue
+++ app.vue
@@ -1,22 +1,9 @@
 <template>
   <NuxtLoadingIndicator color="#FA3E00" :throttle="0" />
   <!-- :throttle="0" -->
-  <div>
+  <main>
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
-  </div>
+  </main>
 </template>
-
-<style>
-body::-webkit-scrollbar {
-  width: 8px;
-  /* 设置滚动条宽度 */
-}
-
-/* 滚动条滑块 */
-body::-webkit-scrollbar-thumb {
-  background-color: #0000007a;
-  /* 设置滑块的颜色 */
-}
-</style>
