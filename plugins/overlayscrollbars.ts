9ed1cc8710fdeed383327167529662b278924538
Simplenty <lishangmingxuan@foxmail.com>
2025-02-08 13:56:12 +0000

    style: 滚动条深色模式适配

--- plugins/overlayscrollbars.ts
+++ plugins/overlayscrollbars.ts
@@ -1,17 +1,32 @@
 import "overlayscrollbars/overlayscrollbars.css";
 import { useOverlayScrollbars } from "overlayscrollbars-vue";
+import { ref } from "vue";
 
 export default defineNuxtPlugin((nuxtApp) => {
   // 检查是否在客户端环境
   if (typeof window !== "undefined" && typeof navigator !== "undefined") {
-    const [initBodyOverlayScrollbars] = useOverlayScrollbars({
-      options: {
+    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+
+    const getScrollbarOptions = (isDark: boolean) => {
+      return {
         scrollbars: {
-          theme: "os-theme-dark",
+          theme: isDark ? "os-theme-light" : "os-theme-dark",
         },
-      },
-    });
-    // 对body使用OverlayScrollbars
+      };
+    };
+
+    const options = ref(getScrollbarOptions(mediaQuery.matches));
+    const [initBodyOverlayScrollbars] = useOverlayScrollbars({ options });
     initBodyOverlayScrollbars(document.body);
+
+    const handleThemeChange = () => {
+      options.value = getScrollbarOptions(mediaQuery.matches);
+    };
+
+    mediaQuery.addEventListener("change", handleThemeChange);
+
+    return () => {
+      mediaQuery.removeEventListener("change", handleThemeChange);
+    };
   }
 });
