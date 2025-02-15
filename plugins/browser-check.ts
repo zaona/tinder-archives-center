0d9f83b7368c7dc0facebf9e9a68b7db29483954
SodiumCode <3409473369@qq.com>
2025-02-16 00:39:58 +0800

    fix(browser-check): 优化 Safari 浏览器版本检测逻辑
    - 改进 Safari 浏览器的检测方法，排除其他浏览器的干扰
    - 修正版本匹配正则，确保正确获取 Safari 版本号
    - 优化代码结构，提高可读性和可维护性

--- plugins/browser-check.ts
+++ plugins/browser-check.ts
@@ -15,10 +15,16 @@ export default defineNuxtPlugin((nuxtApp) => {
 
 // 封装Safari版本检测函数
 function isSafariVersionLessThan(version: number): boolean | null {
-  const safariVersionMatch = /Safari\/(\d+)/.exec(navigator.userAgent);
-  if (!safariVersionMatch) {
+  const userAgent = navigator.userAgent;
+  const isSafari = /^((?!chrome|android).)*safari/i.test(userAgent);
+  if (!isSafari) {
+    return null; // 不是Safari浏览器
+  }
+
+  const versionMatch = /Version\/(\d+)/.exec(userAgent);
+  if (!versionMatch) {
     return null; // 返回null时明确表示未匹配到Safari浏览器，这是一种合理的处理方式
   }
   // 使用parseInt确保比较的是数字类型，而不是字符串类型
-  return parseInt(safariVersionMatch[1]) < version;
+  return parseInt(versionMatch[1]) < version;
 }
