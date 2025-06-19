7aaf33a2a1f64f9623ffb366173e2190990fe834
zaona <3409473369@qq.com>
2025-06-19 20:32:44 +0800

    feat: 更新适配fancybox6

--- components/ImgView.vue
+++ components/ImgView.vue
@@ -1,7 +1,6 @@
 <script setup>
-import { Fancybox } from "@fancyapps/ui/dist/fancybox/fancybox.esm.js";
+import { Fancybox } from "@fancyapps/ui/dist/fancybox/";
 import "@fancyapps/ui/dist/fancybox/fancybox.css";
-import { zh_CN } from "@fancyapps/ui/dist/fancybox/l10n/zh_CN.esm.js";
 
 const props = defineProps({
   imageSrc: {
@@ -15,16 +14,9 @@ const props = defineProps({
 });
 
 Fancybox.bind("[data-fancybox]", {
+  theme: "auto",
   groupAttr: false,
   hideScrollbar: false,
-  l10n: zh_CN,
-  Toolbar: {
-    display: {
-      left: ["infobar"],
-      middle: [],
-      right: ["slideshow", "download", "thumbs", "close"],
-    },
-  },
 });
 </script>
 
@@ -39,64 +31,3 @@ Fancybox.bind("[data-fancybox]", {
     <slot />
   </a>
 </template>
-
-<style>
-.fancybox__container {
-  /* --fancybox-color: #dbdbdb; */
-  --fancybox-color: #0d0d0d;
-  --fancybox-hover-color: #fff;
-  /* --fancybox-bg: rgba(24, 24, 27, 0.98); */
-  --fancybox-bg: rgba(255, 255, 255, 0.9);
-  --fancybox-slide-gap: 10px;
-  --f-spinner-width: 50px;
-  --f-spinner-height: 50px;
-  --f-spinner-color-1: rgba(255, 255, 255, 0.1);
-  --f-spinner-color-2: #bbb;
-  --f-spinner-stroke: 3.65;
-}
-
-.fancybox__toolbar {
-  --f-progress-color: var(--fancybox-color, rgba(255, 255, 255, 0.94));
-  --f-button-width: 46px;
-  --f-button-height: 46px;
-  /* --f-button-color: var(--fancybox-color); */
-  --f-button-color: #dbdbdb;
-  --f-button-hover-color: var(--fancybox-hover-color);
-  /* --f-button-bg: rgba(24, 24, 27, 0.65); */
-  --f-button-bg: rgba(24, 24, 27, 0.9);
-  /* --f-button-hover-bg: rgba(70, 70, 73, 0.65); */
-  --f-button-hover-bg: rgba(70, 70, 73, 0.9);
-  /* --f-button-active-bg: rgba(90, 90, 93, 0.65); */
-  --f-button-active-bg: rgba(90, 90, 93, 0.9);
-  --f-button-border-radius: 0;
-  --f-button-svg-width: 24px;
-  --f-button-svg-height: 24px;
-  --f-button-svg-stroke-width: 1.5;
-  --f-button-svg-filter: drop-shadow(1px 1px 1px rgba(24, 24, 27, 0.15));
-  --f-button-svg-fill: none;
-  --f-button-svg-disabled-opacity: 0.65;
-}
-
-.is-compact .fancybox__footer {
-  /* background: rgba(24, 24, 27, .5); */
-  background: rgba(191, 191, 191, 0.5);
-}
-
-.dark {
-  .fancybox__container {
-    /* --fancybox-color: #dbdbdb; */
-    --fancybox-bg: rgba(128, 128, 128, 0.8);
-  }
-  /* 
-  .fancybox__toolbar {
-    --f-button-color: var(--fancybox-color);
-    --f-button-bg: rgba(24, 24, 27, 0.65);
-    --f-button-hover-bg: rgba(70, 70, 73, 0.65);
-    --f-button-active-bg: rgba(90, 90, 93, 0.65);
-  }
-
-  .is-compact .fancybox__footer {
-    background: rgba(24, 24, 27, 0.5);
-  } */
-}
-</style>
--- components/NormalCard.vue
+++ components/NormalCard.vue
@@ -1,37 +1,38 @@
 <template>
-  <BaseCard>
+  <BaseCard class="group relative">
     <div>
-      <a target="_blank" :href="href" class="group relative">
-        <div class="flex h-full flex-col items-center divide-y divide-gray-200">
-          <div v-if="titleImg" class="h-[56px] w-full truncate p-[20px]">
-            <img class="h-full dark:invert" :src="titleImg" :alt="titleImg" />
-          </div>
-          <p
-            v-if="title"
-            class="w-full shrink-0 truncate p-4 leading-[24px] font-bold text-gray-950"
-            >{{ title }}</p
-          >
-          <imgView
-            v-if="imageSrc"
-            :imageSrc="imageSrc"
-            :name="title"
-            class="h-full w-full p-6"
-          >
-            <img
-              class="h-[100px] w-full dark:invert"
-              :src="imageSrc"
-              :alt="title"
-            />
-          </imgView>
-          <p v-if="description" class="w-full p-4 text-gray-950">{{
-            description
-          }}</p>
+      <div class="flex h-full flex-col items-center divide-y divide-gray-200">
+        <div v-if="titleImg" class="h-[56px] w-full truncate p-[20px]">
+          <img class="h-full dark:invert" :src="titleImg" :alt="titleImg" />
         </div>
-        <BaseIcon
-          v-if="href"
-          name="#icon-arrow"
-          class="absolute top-[18px] right-[18px] size-[20px] text-gray-950 opacity-0 transition-opacity group-hover:opacity-100"
-        />
+        <p
+          v-if="title"
+          class="w-full shrink-0 truncate p-4 leading-[24px] font-bold text-gray-950"
+          >{{ title }}</p
+        >
+        <imgView
+          v-if="imageSrc"
+          :imageSrc="imageSrc"
+          :name="title"
+          class="h-full w-full p-6"
+        >
+          <img
+            class="h-[100px] w-full dark:invert"
+            :src="imageSrc"
+            :alt="title"
+          />
+        </imgView>
+        <p v-if="description" class="w-full p-4 text-gray-950">{{
+          description
+        }}</p>
+      </div>
+      <a target="_blank" :href="href">
+        <div v-if="href" class="absolute top-0 left-0 h-full w-full">
+          <BaseIcon
+            name="#icon-arrow"
+            class="absolute top-[18px] right-[18px] size-[20px] text-gray-950 opacity-0 transition-opacity group-hover:opacity-100"
+          />
+        </div>
       </a>
     </div>
   </BaseCard>
