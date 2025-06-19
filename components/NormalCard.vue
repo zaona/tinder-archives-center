a8863ae3062ca2e499afabf7f2cfaba63c571882
zaona <3409473369@qq.com>
2025-06-19 19:45:30 +0800

    feat: 精简卡片组件

--- components/NormalCard.vue
+++ components/NormalCard.vue
@@ -1,26 +1,38 @@
 <template>
   <BaseCard>
-    <div v-if="href">
+    <div>
       <a target="_blank" :href="href" class="group relative">
-        <NormalCardContent
-          :titleImg="titleImg"
-          :title="title"
-          :imageSrc="imageSrc"
-          :description="description"
-        />
+        <div class="flex h-full flex-col items-center divide-y divide-gray-200">
+          <div v-if="titleImg" class="h-[56px] w-full truncate p-[20px]">
+            <img class="h-full dark:invert" :src="titleImg" :alt="titleImg" />
+          </div>
+          <p
+            v-if="title"
+            class="w-full shrink-0 truncate p-4 leading-[24px] font-bold text-gray-950"
+            >{{ title }}</p
+          >
+          <imgView
+            v-if="imageSrc"
+            :imageSrc="imageSrc"
+            :name="title"
+            class="h-full w-full p-6"
+          >
+            <img
+              class="h-[100px] w-full dark:invert"
+              :src="imageSrc"
+              :alt="title"
+            />
+          </imgView>
+          <p v-if="description" class="w-full p-4 text-gray-950">{{
+            description
+          }}</p>
+        </div>
         <BaseIcon
+          v-if="href"
           name="#icon-arrow"
           class="absolute top-[18px] right-[18px] size-[20px] text-gray-950 opacity-0 transition-opacity group-hover:opacity-100"
         />
       </a>
-    </div>
-    <div v-else>
-      <NormalCardContent
-        :titleImg="titleImg"
-        :title="title"
-        :imageSrc="imageSrc"
-        :description="description"
-      />
     </div>
   </BaseCard>
 </template>
